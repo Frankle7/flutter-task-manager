afd00a786fc5f94c933712b6492717efb549227c
Isfrankle Souza <isfrankle.silva@grupopan.com>
2023-01-27 16:55:43 -0300

    feat(Home): creating logic and styling template

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacidade = true;
 
   @override
   Widget build(BuildContext context) {
@@ -31,14 +38,26 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ),
-        body: ListView(
-          children: [
-            Task('Aprender Flutter', 'assets/logo_flutter.png', 4),
-            Task('Aprender Dart', 'assets/Dart_logo.png', 5),
-            Task('Gerenci. de Task', 'assets/Logo_ger_task.png', 3),
-            Task('Finalizar ONE', 'assets/ONE.png', 2),
-            Task('Iniciar 42', 'assets/logo_42.jpeg', 5),
-          ],
+        body: AnimatedOpacity(
+          opacity: opacidade ? 1 : 0,
+          duration: Duration(milliseconds: 1000),
+          child: ListView(
+            children: [
+              Task('Aprender Flutter', 'assets/logo_flutter.png', 4),
+              Task('Aprender Dart', 'assets/Dart_logo.png', 5),
+              Task('Gerenci. de Task', 'assets/Logo_ger_task.png', 3),
+              Task('Finalizar ONE', 'assets/ONE.png', 2),
+              Task('Iniciar 42', 'assets/logo_42.png', 5),
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              opacidade = !opacidade;
+            });
+          },
+          child: Icon(Icons.remove_red_eye),
         ),
       ),
     );
@@ -66,24 +85,36 @@ class _TaskState extends State<Task> {
       child: Stack(
         children: [
           Container(
-            color: Colors.blue,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(4),
+              color: Colors.blue,
+            ),
             height: 140,
           ),
           Column(
             children: [
               Container(
-                color: Colors.white,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(4),
+                  color: Colors.white,
+                ),
                 height: 100,
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Container(
-                      child: Image.asset(
-                        widget.image,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(4),
+                        color: Colors.black12,
                       ),
                       width: 72,
                       height: 100,
-                      color: Colors.white38,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(4),
+                        child: Image.asset(
+                          widget.image,
+                        ),
+                      ),
                     ),
                     Column(
                       mainAxisAlignment: MainAxisAlignment.center,
@@ -105,27 +136,37 @@ class _TaskState extends State<Task> {
                             Icon(
                               Icons.star,
                               size: 18,
-                              color: (widget.stars >= 1) ? Colors.blue : Colors.blueGrey[900],
+                              color: (widget.stars >= 1)
+                                  ? Colors.blue
+                                  : Colors.blueGrey[900],
                             ),
                             Icon(
                               Icons.star,
                               size: 18,
-                              color: (widget.stars >= 2) ? Colors.blue : Colors.blueGrey[900],
+                              color: (widget.stars >= 2)
+                                  ? Colors.blue
+                                  : Colors.blueGrey[900],
                             ),
                             Icon(
                               Icons.star,
                               size: 18,
-                              color: (widget.stars >= 3) ? Colors.blue : Colors.blueGrey[900],
+                              color: (widget.stars >= 3)
+                                  ? Colors.blue
+                                  : Colors.blueGrey[900],
                             ),
                             Icon(
                               Icons.star,
                               size: 18,
-                              color: (widget.stars >= 4) ? Colors.blue : Colors.blueGrey[900],
+                              color: (widget.stars >= 4)
+                                  ? Colors.blue
+                                  : Colors.blueGrey[900],
                             ),
                             Icon(
                               Icons.star,
                               size: 18,
-                              color: (widget.stars >= 5) ? Colors.blue : Colors.blueGrey[900],
+                              color: (widget.stars >= 5)
+                                  ? Colors.blue
+                                  : Colors.blueGrey[900],
                             ),
                           ],
                         )
@@ -159,9 +200,7 @@ class _TaskState extends State<Task> {
                     child: Container(
                       child: LinearProgressIndicator(
                         color: Colors.white,
-                        value: (widget.stars > 0) 
-                        ? (nivel/widget.stars) 
-                        : 1,
+                        value: (widget.stars > 0) ? (nivel / widget.stars) : 1,
                       ),
                       width: 200,
                     ),
